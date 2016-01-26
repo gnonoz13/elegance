41fe7ac6a960fa0a5925b3f862635ede7aaf019c
kingnono <gnonoz13@gmail.com>
2016-01-26 08:10:58 +0000

    site build

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,23 @@
+class PagesController < ApplicationController
+    
+    def home
+      @contact = Contact.new
+    end
+    
+    def create
+      @contact = Contact.new(contact_params)
+      
+      if @contact.save
+        redirect_to root_path
+      else
+        redirect_to root_path
+      end
+    end
+    
+    private
+    
+      def contact_params
+        params.require(:contact).permit(:name, :email, :comments)
+      end
+
+end
