f007de099c5d0bc63b068b62d04cc3ad4b412b10
George Nono <gnonoz13@gmail.com>
2016-01-27 06:08:14 +0000

    rake to heroku

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,19 @@
 class PagesController < ApplicationController
     
     def new
-      @contact = Contact.new
+        @contact = Contact.new
     end
     
     def create
       @contact = Contact.new(contact_params)
       
       if @contact.save
+        name = params[:contact][:name]
+        email = params[:contact][:email]
+        body = params[:contact][:comments]
+        
+        ContactMailer.contact_email(name, email, body).deliver
+        
         redirect_to root_path
       else
         redirect_to root_path
