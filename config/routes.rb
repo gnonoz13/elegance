67bf6d64cca53a3480f0acfca863369c7de5dfa1
George Nono <gnonoz13@gmail.com>
2016-01-28 07:16:00 +0000

    Starting the profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  
+  resources :users do
+    resource :profile
+  end
+  
   resources :contacts
   root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
